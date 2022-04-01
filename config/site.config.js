25865f83e199e660535134ab61fdb381680159c1
zoike <100909934+zoike@users.noreply.github.com>
2022-04-01 12:00:27 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "ODrive",
+  title: "(*ﾟ▽ﾟ)ﾉ",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/SS',
